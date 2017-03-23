1068b1e126810e57f970efbf75db2306eb159158
administrator <administrator@toolCheckout.dhcp.missouri.edu>
2017-03-23 09:43:22 -0500

    Initial commit

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta http-equiv="content-type" content="text/html; charset=UTF-8">
+    <meta charset="utf-8">
+    <title>MizzouRec Tool Checkout</title>
+    <meta name-"viewport" content="width=device-width, initial-scale=1, maximum-scale=1"
+    <link href="/bootstrap-3.3.7-dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="/css/styles.css" rel="stylesheet">
+    <script src="https://code.jquery.com/jquery-3.2.1.min.js" integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=" crossorigin="anonymous"></script>
+    <script src="bootstrap-3.3.7-dist/js/bootstrap.min.js"></script>
+  </head>
+  <body>
+    <div id="wrap">
+      <div class="navbar navbar-default navbar-fixed-top" role="navigation">
+        <div class="container">
+          <div class="navbar-header">
+            <a class = "navbar-brand" href="http://www.mizzourec.com/"></a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </body>
+</html>
